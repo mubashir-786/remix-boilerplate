72ecc45d0f4dfe11bd1e4333bbda542a518f20e4
razaahmad11 <razaqazhi@gmail.com>
2024-03-11 17:23:34 +0500

    fix: ui

--- app/root.tsx
+++ app/root.tsx
@@ -5,8 +5,14 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/server-runtime";
 import { Analytics } from "@vercel/analytics/react";
+import stylesheet from "~/style/tailwind.css"
+import { cssBundleHref } from "@remix-run/css-bundle";
 
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : [{ rel: "stylesheet", href: stylesheet }]),
+];
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -17,6 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <p className="text-[50px]"></p>
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
 
 import { Form } from "@remix-run/react";
-import type { ActionFunction, MetaFunction } from "@vercel/remix";
+import type { ActionFunction, LinksFunction, MetaFunction } from "@vercel/remix";
 import { useState } from "react";
 import { createUser } from "~/utils/user.server";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -53,82 +54,122 @@ export default function Index() {
       [name]: value
     });
   };
-
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Form method="post" style={{ maxWidth: '500px', margin: '0 auto' }}>
-        <div style={{ marginBottom: '15px' }}>
-          <label style={{ marginRight: '10px' }}>Date:</label>
-          <input type="text" name="date" onChange={handleChange} />
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8", backgroundColor: "#EEEAF9", paddingTop: "20px", paddingBottom: "20px", boxShadow: "30px" }}>
+      <Form method="post" style={{ maxWidth: '700px', margin: '0 auto', backgroundColor: "#F5F6FE", paddingTop: "20px", borderRadius: "10px", paddingBottom: "20px", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)" }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
+          <div>
+            <label className="font-extrabold " style={{ marginRight: '10px' }}>Date:</label>
+          </div>
+
+          <input type="text" name="date" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
-          <label style={{ marginRight: '10px' }}>Provider Name:</label>
-          <input type="text" name="providerName" onChange={handleChange} />
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
+          <div>
+            <label style={{ marginRight: '10px' }}>Provider Name:</label>
+          </div>
+          <input type="text" name="providerName" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Customer Name:</label>
-          <input type="text" name="customerName" onChange={handleChange} />
+          <div>
+            <input type="text" name="customerName" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Contact Number:</label>
-          <input type="text" name="contactNumber" onChange={handleChange} />
+          <div>
+            <input type="text" name="contactNumber" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Associated Phone Number:</label>
-          <input type="text" name="associatedPhoneNumber" onChange={handleChange} />
+          <div>
+            <input type="text" name="associatedPhoneNumber" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Service Address:</label>
-          <input type="text" name="serviceAddress" onChange={handleChange} />
+          <div>
+            <input type="text" name="serviceAddress" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Bill Amount:</label>
-          <input type="text" name="billAmount" onChange={handleChange} />
+          <div>
+            <input type="text" name="billAmount" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Card Number:</label>
-          <input type="text" name="cardNumber" onChange={handleChange} />
+          <div>
+            <input type="text" name="cardNumber" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Expiration:</label>
-          <input type="text" name="expiration" onChange={handleChange} />
+          <div>
+            <input type="text" name="expiration" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>CVV:</label>
-          <input type="text" name="cvv" onChange={handleChange} />
+          <div>
+            <input type="text" name="cvv" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Billing Address:</label>
-          <input type="text" name="billingAddress" onChange={handleChange} />
+          <div>
+            <input type="text" name="billingAddress" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Email:</label>
-          <input type="text" name="email" onChange={handleChange} />
+          <div>
+            <input type="text" name="email" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Sales Agent Name:</label>
-          <input type="text" name="salesAgentName" onChange={handleChange} />
+          <div>
+            <input type="text" name="salesAgentName" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div style={{ marginBottom: '15px' }}>
+        <div style={{ marginBottom: '15px', textAlign: "center" }}>
           <label style={{ marginRight: '10px' }}>Categories:</label>
-          <input type="text" name="categories" onChange={handleChange} />
+          <div style={{ border: "2px", borderColor: "black" }}>
+            <input type="text" name="categories" style={{ width: "90%", height: "30px", marginTop: "10px", borderRadius: "10px", outline: "none", border: "none", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", paddingLeft: "20px" }} onChange={handleChange} />
+          </div>
+
         </div>
 
-        <div>
-          <button name="action" value="create" type="submit" style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Submit</button>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button name="action" value="create" type="submit" style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '10px', cursor: 'pointer', width: "94%", boxShadow: "-1px 0 5px 0 rgba(0, 0, 0, .5)", marginTop: "20px" }}>Submit</button>
         </div>
       </Form>
     </div>
